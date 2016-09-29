c7b84370b701869819b77540ac28eb739bfd9fc8
Juan Carlos Carbajal Ipenza <goesrex@gmail.com>
2016-09-29 07:58:50 -0500

    Initial Commit

---
+++ Accediendo-a-la-nube-con-iOS_Semana-3_PruebaGUI WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Accediendo-a-la-nube-con-iOS_Semana-3_PruebaGUI WatchKit Extension
+//
+//  Created by Juan Carlos Carbajal Ipenza on 29/09/16.
+//  Copyright © 2016 Juan Carlos Carbajal Ipenza. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
